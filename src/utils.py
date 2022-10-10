daf56f302d0654877dfb785afea49250689716d4
yimin <zgjxym@163.com>
2022-10-10 13:44:19 +0800

    local regis

--- src/utils.py
+++ src/utils.py
@@ -11,8 +11,8 @@ from glob import glob
 import tarfile
 from tqdm import tqdm
 import numpy as np
-
-
+from arosics import COREG_LOCAL, DESHIFTER
+import shutil
 from pansharpen import gdal_pansharpen
 
 logging.getLogger().setLevel('INFO')
@@ -27,7 +27,8 @@ class gf2:
                  dem:str,
                  pan_res=8e-06,
                  mul_res=3.2e-05,
-
+                 correct_type = 0,    # 0 means global,   1 means local
+                 cpus = None          # 防止bug
                  ):
         self.gf2 = gf2_path
         self.save = out_dir
@@ -87,7 +88,7 @@ class gf2:
         gdal_pansharpen(["pass", pan_path, mul_path, out_path])
         logging.info("===完成(Done)!")
 
-    def __regis(self, image, out):
+    def __regis(self, image, out, datatype=0, correct_type=1):
         """
         registration.
         :param image:
@@ -96,9 +97,63 @@ class gf2:
         """
         info = "===图像配准(Registration.): \n{} starting".format(image)
         logging.info(info)
-        CR = COREG(self.ref, image, ws=(1024, 1024), max_shift=100, max_iter=10000, path_out=out)
-        CR.calculate_spatial_shifts()
-        CR.correct_shifts()
+        result_path = []
+
+        if datatype == 0:
+            # PAN
+            regis_image = image
+        elif datatype == 1:
+            #MSS
+            regis_image = self.__tif_split(image, self.gf2)
+        else:
+            raise Exception("datatype error!")
+
+        if correct_type == 0:
+            if isinstance(regis_image, str):
+                CR = COREG(self.ref, regis_image, ws=(1024, 1024), max_shift=200,
+                           max_iter=10000, path_out=out)
+                CR.correct_shifts()
+
+            else:
+                CR = COREG(self.ref, regis_image[0], ws=(1024, 1024), max_shift=200,
+                           max_iter=10000)
+                CR.calculate_spatial_shifts()
+                for image in regis_image:
+                    new_list = image.replace('.tif', '_regis.tif')
+                    result_path.append(new_list)
+                    self.garbage.append(new_list)
+                    DESHIFTER(image, CR.coreg_info, path_out=new_list).correct_shifts()
+                self.__concate_tif(result_path, out)
+
+        elif correct_type == 1:
+            if isinstance(regis_image, str):
+                CR = COREG_LOCAL(self.ref, regis_image, 400, window_size=(512, 512),
+                                 max_shift=200, max_iter=1000, path_out=out,
+                                 # CPUs=1
+                                 )
+                CR.correct_shifts()
+            else:
+                CR = COREG_LOCAL(self.ref, regis_image[0], 200, window_size=(256, 256),
+                                 max_shift=200, max_iter=1000,
+                                 # CPUs=1
+                                 )
+                for image in regis_image:
+                    new_list = image.replace('.tif', '_regis.tif')
+                    result_path.append(new_list)
+                    self.garbage.append(new_list)
+                    DESHIFTER(image, CR.coreg_info, path_out=new_list).correct_shifts()
+                self.__concate_tif(result_path, out)
+
+        else:
+            raise Exception("Error correct_type!! \n"
+                            "correct_type must be 0 or 1 \n"
+                            "0: global regif; 1: local regis!")
+
+        # 配准输出
+
+        del image
+        del out
+        del CR
         logging.info("===完成(Done)!")
 
     def init_path(self):
@@ -110,8 +165,6 @@ class gf2:
             else:
                 for zipfile in zipfiles:
                     self._dir.append(self.unpackage(zipfile))
-        elif self.gf2.endswith(".tar.gz"):
-            self._dir.append(self.unpackage(self.gf2))
 
     def correct(self):
         if len(self._dir) == 0:
@@ -120,7 +173,6 @@ class gf2:
                             "Maybe check whether input GF2 path is correct.\n")
         for gf2_dir in self._dir:
             logging.info(gf2_dir)
-            print(gf2_dir)
             print(self._dir[0])
             pan_path = glob(gf2_dir + "/*PAN*.tiff")[0]
             mss_path = glob(gf2_dir + "/*MSS*.tiff")[0]
@@ -137,9 +189,8 @@ class gf2:
             # step 2 图像配准
             pan_path_orth_reg = pan_path_orth.replace(".tiff", "_regis.tiff")
             mss_path_orth_reg = mss_path_orth.replace(".tiff", "_regis.tiff")
-
-            # linux上，先校正pan，后mss,会killed，64GB oom
-            self.__regis(mss_path_orth, mss_path_orth_reg)
+            
+            self.__regis(mss_path_orth, mss_path_orth_reg, datatype=1)
             self.__regis(pan_path_orth, pan_path_orth_reg)
 
             self.garbage.append(pan_path_orth_reg)
@@ -215,6 +266,41 @@ class gf2:
         """
         return gdal_dataset.GetRasterBand(index)
 
+    def writeTiff(self, im_data, im_geotrans, im_proj, path):
+        """
+        :param im_data: 需要保存的数组
+        :param im_geotrans:
+        :param im_proj:
+        :param path: 输出路径
+        :return:
+        """
+        if 'int8' in im_data.dtype.name:
+            datatype = gdal.GDT_Byte
+        elif 'int16' in im_data.dtype.name:
+            datatype = gdal.GDT_UInt16
+        else:
+            datatype = gdal.GDT_Float32
+        if len(im_data.shape) == 3:
+            im_bands, im_height, im_width = im_data.shape
+        elif len(im_data.shape) == 2:
+            im_data = np.array([im_data])
+            im_bands, im_height, im_width = im_data.shape
+
+        # 创建文件
+        driver = gdal.GetDriverByName("GTiff")
+        dataset = driver.Create(path, int(im_width), int(im_height), int(im_bands), datatype)
+
+        if (dataset != None):
+            dataset.SetGeoTransform(im_geotrans)  # 写入仿射变换参数
+            dataset.SetProjection(im_proj)  # 写入投影
+            for i in range(im_bands):
+                dataset.GetRasterBand(i + 1).WriteArray(im_data[i])
+        else:
+            print("保存失败！")
+
+        del dataset
+        del driver
+
     def _read_tif(self, image_path):
         image_dataset = gdal.Open(image_path)
         assert image_dataset is not None, "{} open error!".format(image_path)
@@ -224,7 +310,72 @@ class gf2:
         image_array *= 255.0 / image_array.max()
         return image_array.astype(type)
 
+    def __tif_split(self, image_path, out_dir):
+        tiff_gdal = self._read_tif(image_path)
+        GeoTrans = tiff_gdal.GetGeoTransform()
+        GeoProj = tiff_gdal.GetProjection()
+        channels = tiff_gdal.RasterCount
 
+        split_lists = []
+
+        for order in range(channels):
+            single_band = self.__get_single_band(tiff_gdal, order + 1).ReadAsArray().astype(float)
+            save_path = os.path.join(out_dir, 'band_' + str(order) + '.tif')
+            split_lists.append(save_path)
+            self.garbage.append(save_path)
+            self.writeTiff(single_band, GeoTrans, GeoProj, save_path)
+            del single_band
+        del tiff_gdal
+        return split_lists
+
+    def __concate_tif(self, tif_lists, out_path):
+        information = gdal.Open(tif_lists[0])
+        width = information.RasterXSize
+        height = information.RasterYSize
+        geotrans = information.GetGeoTransform()
+        projection = information.GetProjection()
+
+        # dtype = np.int16
+        channel = len(tif_lists)
+
+        # 创建文件
+        driver = gdal.GetDriverByName("GTiff")
+        dataset = driver.Create(out_path, int(width), int(height), int(channel), gdal.GDT_UInt16)
+
+        if (dataset != None):
+            dataset.SetGeoTransform(geotrans)  # 写入仿射变换参数
+            dataset.SetProjection(projection)  # 写入投影
+            for i in range(channel):
+                information = gdal.Open(tif_lists[i])
+                old_array = information.ReadAsArray(0, 0, width, height)
+                dataset.GetRasterBand(i + 1).WriteArray(old_array)
+        else:
+            print("保存失败！")
+
+
+if __name__ == '__main__':
+    # # gf2 = r"D:\gf2_py6s\GF2_PMS2_E132.3_N33.5_20170422_L1A0002321961.tar.gz"
+    # gf2_path = r"E:\python\DL_ZX\GF2_building\zhengzhou\zhengzhou"
+    # out = r"F:\preprocess_zhengzhou_test"
+    # dem = r"E:\python\DL_ZX\GF2_building\gf2\N34E113\ASTGTM2_N34E113\ASTGTM2_N34E113_dem.tif"
+    # ref = r"F:\zhengzhou_gf_googleearth_resample.tif"
+    #
+    # pre = gf2(gf2_path, out, ref, dem)
+    # pre.init_path()
+    # pre.correct()
+    # pre.target_output([3, 2, 1])
+    #
+
+    gf2_path = r"/home/xiaxiaoyun/yimin/data_fuzhu/gf2/zhengzhou"
+    out = r"/home/xiaxiaoyun/yimin/data_fuzhu"
+    ref = r"/home/xiaxiaoyun/yimin/data_fuzhu/ref_high_res_ge/zhengzhou_gf_googleearth_resample.tif"
+    dem = r"/home/xiaxiaoyun/yimin/data_fuzhu/dem/ASTGTM2_N34E113_dem.tif"
+
+
+    pre = gf2(gf2_path, out, ref, dem)
+    pre.init_path()
+    pre.correct()
+    pre.target_output([3, 2, 1])
 
 
 
